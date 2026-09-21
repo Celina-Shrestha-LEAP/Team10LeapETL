b6952e493a74e6f37a50d42c29df7c0a3b0122e5
Celina-Shrestha-LEAP <celinaa.shresthaa44@gmail.com>
2026-09-21 19:34:49 +0000

    error handling for db connect

--- src/db/source.py
+++ src/db/source.py
@@ -6,10 +6,16 @@ load_dotenv()
 
 
 def get_source_connection():
-    return psycopg.connect(
-        host=os.getenv("SOURCE_DB_HOST"),
-        port=os.getenv("SOURCE_DB_PORT"),
-        dbname=os.getenv("SOURCE_DB_NAME"),
-        user=os.getenv("SOURCE_DB_USER"),
-        password=os.getenv("SOURCE_DB_PASSWORD"),
-    )
+    try:
+        conn = psycopg.connect(
+            host=os.getenv("SOURCE_DB_HOST"),
+            port=os.getenv("SOURCE_DB_PORT"),
+            dbname=os.getenv("SOURCE_DB_NAME"),
+            user=os.getenv("SOURCE_DB_USER"),
+            password=os.getenv("SOURCE_DB_PASSWORD"),
+        )
+        print("Trading database connection successful")
+        return conn
+    except Exception as e:
+        print(f"Trading database connection error: {e}")
+        raise
--- src/db/target.py
+++ src/db/target.py
@@ -6,10 +6,16 @@ load_dotenv()
 
 
 def get_target_connection():
-    return psycopg.connect(
-        host=os.getenv("TARGET_DB_HOST"),
-        port=os.getenv("TARGET_DB_PORT"),
-        dbname=os.getenv("TARGET_DB_NAME"),
-        user=os.getenv("TARGET_DB_USER"),
-        password=os.getenv("TARGET_DB_PASSWORD"),
-    )
+    try:
+        conn = psycopg.connect(
+            host=os.getenv("TARGET_DB_HOST"),
+            port=os.getenv("TARGET_DB_PORT"),
+            dbname=os.getenv("TARGET_DB_NAME"),
+            user=os.getenv("TARGET_DB_USER"),
+            password=os.getenv("TARGET_DB_PASSWORD"),
+        )
+        print("Datawarehouse connection successful")
+        return conn
+    except Exception as e:
+        print(f"Datawarehouse connection error: {e}")
+        raise
